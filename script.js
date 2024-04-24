1ea14e029d8d2c4a68786248dbc278bc841fbab0
Ilya30432 <IlyaS30432@gmail.com>
2024-04-24 21:54:33 +0200

    correction of error with number

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const spanName = document.querySelector('.names')
 const spanNumber = document.querySelector('.number')
 const spanEmail = document.querySelector('.emails')
 const checkName =/^[A-Z][a-z]{1,} [A-Z][a-z]{1,} [A-Z][a-z]{1,}$/
-const checkNumber =/^\+?(\(\d{3}\))?(?:-?\d){7,15}$/
+const checkNumber = /^(?:\+\(380\)|0)\d{2}\-?\d{2}\-?\d{3}\-?\d{2}$/;
 const checkEmail = /^[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]{2,}$/;
 
 
@@ -14,26 +14,36 @@ bth.addEventListener('click', function(){
     form.style.display = "block"
 });
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    productForm.textContent = ""
-    const formData = new FormData(this);
-    const formObject = {};
-    formData.forEach(function (value, key) {
-            formObject[key] = value
-    }) 
-    const name = formObject['name']
-    const num = formObject['number_phone']
-    const email = formObject['email']
-    spanName.textContent = !checkName.test(name) ? 'invalid' : 'valid';
-    spanNumber.textContent = !checkNumber.test(num) ? 'invalid' : 'valid';
-    spanEmail.textContent = !checkEmail.test(email) ? 'invalid' : 'valid';
-    
-    if (checkName.test(name) && checkNumber.test(num) && checkEmail.test(email)){
-    for (const key in formObject){
-        const newDiv = document.createElement('div')
-        newDiv.textContent = `${key}: ${formObject[key]}`;
-        productForm.appendChild(newDiv)
-        }
-   }
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  productForm.textContent = "";
+  const formData = new FormData(this);
+  const formObject = {};
+  formData.forEach(function (value, key) {
+    formObject[key] = value;
+  });
+  const name = formObject["name"];
+  const num = formObject["number_phone"];
+  const email = formObject["email"];
+  function validateField(value, regex, errorClass) {
+    const isValid = regex.test(value);
+    if (!isValid) {
+      errorClass.textContent = "invalid";
+    }else{
+        errorClass.textContent =""
+    }
+    return isValid;
+  }
+
+  if (
+    validateField(name, checkName, spanName) &&
+    validateField(num, checkNumber, spanNumber) &&
+    validateField(email, checkEmail, spanEmail)
+  ) {
+    for (const key in formObject) {
+      const newDiv = document.createElement("div");
+      newDiv.textContent = `${key}: ${formObject[key]}`;
+      productForm.appendChild(newDiv);
+    }
+  }
 });
